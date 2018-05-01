411f8004e5d13661bdcc56fa51c9393d4f76c6b1
Ajahne Santa Anna <ajahne@gmail.com>
2018-05-01 17:31:25 -0400

    Create example.js

---
+++ child_process/spawn/example.js
@@ -0,0 +1,15 @@
+//from docs: https://nodejs.org/docs/latest-v6.x/api/child_process.html
+const spawn = require('child_process').spawn;
+const ls = spawn('ls', ['-lh', '/usr']);
+
+ls.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on('data', (data) => {
+  console.log(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
