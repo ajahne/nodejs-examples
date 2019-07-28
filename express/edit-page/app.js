ad1a2684c9ab07ab429b724c509caa11482e70fe
ajahne <ajahne@gmail.com>
2019-07-27 23:36:47 -0400

    added ability to write a file and handle json requests

--- express/edit-page/app.js
+++ express/edit-page/app.js
@@ -1,14 +1,30 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3001;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const writeFile = (data) => {
+  fs.writeFile('result.json', data.toString(), (err) => {
+    if (err) {
+      console.log('error');
+    }
+    console.log('file written');
+  });
+}
 
 app.use(express.static('html'));
 app.use(express.static('scripts'));
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.get('/', (req, res) => res.send('Hello World!'));
 
 app.put('*', (req, res) => {
-  console.log('editing happening');
+  console.log('PUT: editing happening');
+  // console.log(req);
+  console.log(req.body);
+  console.log(res.body);
+  writeFile(req);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
