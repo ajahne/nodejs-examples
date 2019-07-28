d6b7530519fdbb7d5fa4e778fa956ed377b0d6a7
ajahne <ajahne@gmail.com>
2019-07-28 00:18:05 -0400

    append to file on put

--- express/edit-page/app.js
+++ express/edit-page/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
+const appendFile = require('./append-file.js');
+
 const app = express();
 const port = 3001;
 
@@ -24,7 +27,10 @@ app.put('*', (req, res) => {
   // console.log(req);
   console.log(req.body);
   res.send('successful put');
-  writeFile(req);
+  // writeFile(req);
+
+  const filename = path.join(__dirname + '/html/index.html');
+  appendFile(filename, '<h3>Updated!</h3>');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
