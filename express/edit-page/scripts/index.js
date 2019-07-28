be75e38ccb5fab1cbdf3459d2dbfc91b9e887046
ajahne <ajahne@gmail.com>
2019-07-27 23:37:42 -0400

    minor updates and slight refactoring

--- express/edit-page/scripts/index.js
+++ express/edit-page/scripts/index.js
@@ -1,27 +1,36 @@
 const save = () => {
-  const url = "http://localhost:3001/";
-
   const request = new XMLHttpRequest();
-  request.open("PUT", url, true);
+  handleRequest(request);
+  request.open("PUT", getUrl(), true);
   request.setRequestHeader('Content-type','application/json; charset=utf-8');
+
   request.send(getData());
 }
 
+const getUrl = () => {
+  const url = "http://localhost:3001/";
+  return url;
+}
+
 const getData = () => {
-  var data = {};
+  console.log('getData');
+  const data = {};
   data.firstname = "John2";
   data.lastname  = "Snow2";
-  var json = JSON.stringify(data);
+  const json = JSON.stringify(data);
   return json;
 }
 
-//
-// xhr.onload = function () {
-// 	var users = JSON.parse(xhr.responseText);
-// 	if (xhr.readyState == 4 && xhr.status == "200") {
-// 		console.table(users);
-// 	} else {
-// 		console.error(users);
-// 	}
-// }
-// xhr.send(json);
+const handleRequest = (xhr) => {
+  console.log('handleRequest');
+  xhr.onload = function () {
+    console.log('xhr');
+  	var users = JSON.parse(xhr.responseText);
+  	if (xhr.readyState == 4 && xhr.status == "200") {
+  		console.table(users);
+  	} else {
+  		console.error(users);
+  	}
+  }
+  // xhr.send(json);
+}
