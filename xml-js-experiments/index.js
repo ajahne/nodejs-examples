5f50b334b16388950c4a8161b94ed77e6b1269cc
Ajahne Santa Anna <ajahne@gmail.com>
2019-08-23 21:11:20 -0400

    added missing semicolon

--- xml-js-experiments/index.js
+++ xml-js-experiments/index.js
@@ -1,5 +1,5 @@
 const parseString = require('xml2js').parseString;
-const xml = "<root>Hello xml2js!</root>"
+const xml = "<root>Hello xml2js!</root>";
 
 parseString(xml, function (err, result) {
     console.dir(result);
