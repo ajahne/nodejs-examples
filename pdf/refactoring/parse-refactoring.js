14009ed37b98ce02c889e8258e2c24d4f64986f1
ajahne <ajahne@gmail.com>
2019-08-25 02:00:54 -0400

    broken, but committing to have a saved version

--- pdf/refactoring/parse-refactoring.js
+++ pdf/refactoring/parse-refactoring.js
@@ -3,14 +3,20 @@ const writeFile = require('../../express/edit-page/write-file.js');
 
 const pdfPath = '../assets/refactoring-improving-existing-2nd.pdf';
 const loadingTask = pdfjsLib.getDocument(pdfPath);
-const pageNumber = 2;
+// const pageNumber = 3;
 const outputFile = '../assets/refactoring.txt';
 
+let text = '';
+// let lastPromise;
+
+let promises = [];
 loadingTask.promise.then(function(doc) {
   const numPages = doc.numPages;
+
   const loadPage = function(pageNum) {
+    console.log(`loadPage ${pageNum}`);
+    let textContent;
     return doc.getPage(pageNum).then(function(page) {
-      let textContent;
       return page.getTextContent().then(function(content) {
         const strings = content.items.map(function(item, index, array) {
           //.transform[5] is the y position of the current line
@@ -21,13 +27,38 @@ loadingTask.promise.then(function(doc) {
           }
           return str;
         });
-        console.log('## Text Content');
+        // console.log('## Text Content');
         textContent = strings.join('');
-        console.log(textContent);
+        // console.log('returning text content');
+        // console.log(textContent);
+        return textContent;
       }).then(function() {
-        writeFile(outputFile, textContent);
+        console.log('***then time***');
+        text += textContent;
+        return text;
+        //writeFile(outputFile, textContent);
+        // writeFile(outputFile, text);
       });
     });
   };
-  loadPage(pageNumber);
-});
+
+  let bound;
+  let promise;
+  for (let i = 1; i <= 10; i++) {
+    // console.log('looping...');
+    // lastPromise = lastPromise.then(loadPage.bind(null, i));
+    bound = loadPage.bind(null, i);
+    promise = bound().then(function() {
+      // console.log('hey, no different than a callback!');
+    });
+    promises.push(promise);
+  }
+  // return promise;
+  Promise.all(promises).then(function(values) {
+    console.log(values);
+    // console.log(text);
+    writeFile(outputFile, text);
+  });
+}).then(function(){
+  console.log('end of document');
+})
