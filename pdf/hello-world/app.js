70815630861791a1b3351589f307609cfbbbbf31
ajahne <ajahne@gmail.com>
2019-08-24 21:07:02 -0400

    updated to handle newlines

--- pdf/hello-world/app.js
+++ pdf/hello-world/app.js
@@ -5,30 +5,37 @@ const pdfPath = 'Hello World.pdf';
 const loadingTask = pdfjsLib.getDocument(pdfPath);
 
 loadingTask.promise.then(function(doc) {
-  var numPages = doc.numPages;
+  const numPages = doc.numPages;
   console.log('# Document Loaded');
   console.log('Number of Pages: ' + numPages);
   console.log();
   // console.log(doc);
 
-  var loadPage = function (pageNum) {
-    return doc.getPage(pageNum).then(function (page) {
+  const loadPage = function(pageNum) {
+    return doc.getPage(pageNum).then(function(page) {
       console.log('# Page ' + pageNum);
       let textContent;
-      return page.getTextContent().then(function (content) {
+
+      return page.getTextContent().then(function(content) {
         // Content contains lots of information about the text layout and
         // styles, but we need only strings at the moment
-        var strings = content.items.map(function (item) {
-          console.log('item.str;' + item.str);
-          return item.str;
+
+        const strings = content.items.map(function(item, index, array) {
+          // console.log(`item: ${item.str}, ${item.transform[5]}`);
+          //.transform[5] is the y position of the current line
+          //if a new "y", then its a new line, so add newline character
+          let str = item.str;
+          if (index === 0 || item.transform[5] !== array[index-1].transform[5]) {
+            str += '\n';
+          }
+          // return item.str;
+          return str;
         });
         console.log('## Text Content');
-        textContent = strings.join(' ');
+        textContent = strings.join('');
         console.log(textContent);
-      }).then(function () {
+      }).then(function() {
         writeFile('file.txt', textContent);
-        console.log('done');
-        console.log();
       });
     });
   };
