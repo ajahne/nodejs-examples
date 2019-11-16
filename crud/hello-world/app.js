764af3072607db8bc29e9b21b699bc3c458e4997
ajahne <ajahne@gmail.com>
2019-11-16 16:44:42 -0500

    now serves up index.html

--- crud/hello-world/app.js
+++ crud/hello-world/app.js
@@ -1,6 +1,11 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello CRUD!'));
+app.get('/home', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
 app.listen(port, () => console.log(`listening on port ${port}`));
