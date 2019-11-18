f669661945cde7bb0170e92e0065cf47f8b62b06
ajahne <ajahne@gmail.com>
2019-11-17 20:29:03 -0500

    now adds first and last name to db

--- crud/hello-world/app.js
+++ crud/hello-world/app.js
@@ -4,6 +4,11 @@ const mongoClient = require('./mongo-client.js');
 const app = express();
 const port = 3000;
 
+// for parsing application/json
+app.use(express.json());
+// for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => res.send('Hello CRUD!'));
 app.get('/home', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -12,7 +17,10 @@ app.get('/home', (req, res) => {
 app.post('/addname', (req, res) => {
   console.log('post happened');
   console.log(req.body);
-  mongoClient.addName('test-first-name', 'test-last-name');
+  console.log(req.body.firstname);
+  console.log(req.body.lastname);
+  
+  mongoClient.addName(req.body.firstname, req.body.lastname);
   res.send('success');
 });
 
